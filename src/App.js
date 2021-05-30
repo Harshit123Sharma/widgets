0ac2e5e8a6b80709cffdcbdb72e5c504e01ce7bd
harshit2001sharma@gmail.com <44293679+Harshit123Sharma@users.noreply.github.com>
2021-05-30 22:57:45 +0530

    Setting up the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 /*jshint esversion: 6 */
 import "./App.css";
 import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+const items = [
+  {
+    title: "A",
+    content: "B",
+  },
+  {
+    title: "C",
+    content: "D",
+  },
+  {
+    title: "E",
+    content: "F",
+  }
+];
 
 export default function App() {
 
   return (
     <div>
-      
+      {/*<Accordion items={items} />*/}
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const Search = () => {
+
+    const [term, setTerm] = useState("");
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                    className="input" 
+                    placeholder="Search"
+                    value={term}
+                    onChange={e => setTerm(e.target.value)}
+                     />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
