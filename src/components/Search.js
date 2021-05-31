45a51ab4f88377d77c6bd62d506a061f6bdf3bd5
harshit2001sharma@gmail.com <44293679+Harshit123Sharma@users.noreply.github.com>
2021-05-31 22:37:27 +0530

    Added WikiSearch Widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
 
-    const [term, setTerm] = useState("");
+    const [term, setTerm] = useState("program");
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerID = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerID);
+        };
+    }, [term]);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+                params: {
+                    action: "query",
+                    list: "search",
+                    origin: "*",
+                    format: "json",
+                    srsearch: debouncedTerm,
+                },
+            });
+            setResults(data.query.search);
+        };
+        search();
+    }, [debouncedTerm]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                        >
+                            GO
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet}} />
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -14,8 +64,16 @@ const Search = () => {
                     placeholder="Search"
                     value={term}
                     onChange={e => setTerm(e.target.value)}
+                    /*{ onKeyDown={e => e.key === "Enter" && handle} }
+                        const handle = () => {
+                        if (term) {
+                        search();
+                        }        }*/
                      />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
